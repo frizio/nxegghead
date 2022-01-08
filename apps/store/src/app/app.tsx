6b02a07ae73d95c9f61bf9ed3f60ab32a5892adf
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2022-01-08 15:45:28 +0100

    Create React Feature Libraries in an Nx Workspace

--- apps/store/src/app/app.tsx
+++ apps/store/src/app/app.tsx
@@ -7,24 +7,30 @@ import { getAllGames } from '../fake-api';
 
 import { Header } from '@nxegghead/store/ui-shared';
 import { formatRating } from '@nxegghead/store/util-formatters';
+import { StoreFeatureGameDetail } from '@nxegghead/store/feature-game-detail';
 
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
-import { Route, Link } from 'react-router-dom';
 
-import { StoreFeatureGameDetail } from '@nxegghead/store/feature-game-detail';
+import { Route, useHistory } from 'react-router-dom';
+
 
 export const App = () => {
+  const history = useHistory();
   return (
     <>
       <Header />
       <div className="container">
         <div className="games-layout">
           {getAllGames().map((x) => (
-            <Card key={x.id} className="game-card">
+            <Card
+              key={x.id}
+              className="game-card"
+              onClick={() => history.push(`/game/${x.id}`)}
+            >
               <CardActionArea>
                 <CardMedia
                   className="game-card-media"
@@ -57,46 +63,8 @@ export const App = () => {
         </div>
       </div>
 
-      {/* START: routes */}
-      {/* These routes and navigation have been generated for you */}
-      {/* Feel free to move and update them to fit your needs */}
-      <br />
-      <hr />
-      <br />
-      <div role="navigation">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/feature-game-detail">StoreFeatureGameDetail</Link>
-          </li>
-          <li>
-            <Link to="/page-2">Page 2</Link>
-          </li>
-        </ul>
-      </div>
-      <Route
-        path="/"
-        exact
-        render={() => (
-          <div>
-            This is the generated root route.{' '}
-            <Link to="/page-2">Click here for page 2.</Link>
-          </div>
-        )}
-      />
-      <Route path="/feature-game-detail" component={StoreFeatureGameDetail} />
-      <Route
-        path="/page-2"
-        exact
-        render={() => (
-          <div>
-            <Link to="/">Click here to go back to root page.</Link>
-          </div>
-        )}
-      />
-      {/* END: routes */}
+      <Route path="/game/:id" component={StoreFeatureGameDetail} />
+
     </>
   );
 };
